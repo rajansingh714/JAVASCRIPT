8f80bdb3e9dff87f9c03b0dba7fca6599ed03f1f
Your Name <rajansingh70707@gmail.com>
2025-09-08 12:49:54 +0530

    added strcitEquality file

---
+++ abscract, strict, NaN/Strict_equality_operator.js
@@ -0,0 +1,15 @@
+console.log(NaN === 7);
+console.log(NaN === NaN);
+console.log(7 === 7);
+console.log(17 === 7);
+console.log(0 === -0);
+console.log(-0 === 0);
+console.log(null === null);
+console.log(undefined === undefined);
+console.log("rajan" === "rajan");
+// console.log({} === {}) it will give a false
+let x = {};
+console.log(x === x);
+console.log(Symbol("rajan") === Symbol("rajan"));
+let z = "singh";
+console.log(z === z);
